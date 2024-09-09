a35c52d723ee3c5c2f182215892d5d6d54f49c65
dormieriancitizen <dormieriancitizen@gmail.com>
2024-09-08 21:56:34 -0400

    instance picker is a inquierier thing wow fancy

--- sheet_manager.py
+++ sheet_manager.py
@@ -11,3 +11,12 @@ def get_modlist_info(instance):
     sh = get_spreadsheet()
     instance_sheet = sh.worksheet(instance)
     return instance_sheet.acell("E2").value
+
+def get_instances():
+    sh = get_spreadsheet()
+    sheets = [sheet.title for sheet in sh.worksheets()]
+    
+    unwanted = ["Mods","SlowModsComparer","ModlistComparer","Instance Template"]
+    sheets = [sheet for sheet in sheets if sheet not in unwanted ]
+
+    return sheets
