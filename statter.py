bdc585afd2a85735ac544e82460eacc2bf4c9a6e
dormieriancitizen <dormieriancitizen@gmail.com>
2024-09-12 20:49:48 -0400

    Added dependency statting

--- statter.py
+++ statter.py
@@ -1,4 +1,4 @@
-import mod_parser
+import mod_parser, click
 
 def getModsBy(modInfo,x):
     modStat = { (mod["title"] if "title" in mod else f"missing-title"):int((mod[x] if x  in mod else 0)) for mod in modInfo["response"]["publishedfiledetails"]}
@@ -22,3 +22,43 @@ def get_common_mod_authors(modInfo):
     modAuthors = {k: v for k, v in sorted(modAuthors.items(), key=lambda item: item[1])}
     print("".join([f"{author} has made {modAuthors[author]} mods \n" for author in modAuthors]))
     return
+
+def get_common_mod_dependencies(modInfo):
+    modDeps = {}
+    for mod in modInfo["response"]["publishedfiledetails"]:
+        try:
+            mod_about = mod_parser.mod_about(mod["publishedfileid"])
+            if "modDependencies" in mod_about["ModMetaData"]:
+                try:
+                    dep = mod_about["ModMetaData"]["modDependencies"]["li"]
+                    if "packageId" in dep:
+                        dependencies = [dep["packageId"]]
+                    else:
+                        dependencies = [x["packageId"] for x in dep]
+                except TypeError:
+                    dependencies = []
+            else:
+                dependencies = []
+        except (KeyError, AttributeError) as err:
+            print(err)
+            dependencies = ["unknown"]
+        
+        for dependency in dependencies:
+            if dependency in modDeps:
+                try:
+                    modDeps[dependency].append(mod_about["ModMetaData"]["name"])
+                except KeyError:
+                    modDeps[dependency].append("Unknown Mod")
+            else:
+                try:
+                    modDeps[dependency] = [mod_about["ModMetaData"]["name"]]
+                except KeyError:
+                    modDeps[dependency] = ["Unknown Mod"]
+    modDeps = {k: v for k, v in sorted(modDeps.items(), key=lambda item: item[1])}
+    print("".join([f"{dependency} has {len(modDeps[dependency])} dependents \n" for dependency in modDeps]))
+
+    if click.confirm("Get dependents of specific mod?"):
+        mod = click.prompt("Which mod? (give id)")
+        print("\n".join(modDeps[mod]))
+
+    return 
